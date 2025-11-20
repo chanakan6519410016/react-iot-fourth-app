10a729800bbb4324bd3fe9f6934a91eebc2d198b
chanakan6519410016 <s6519410016@sau.ac.th>
2025-11-20 12:50:06 +0700

    v.2

--- src/libs/supabaseClient.js
+++ src/libs/supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from "npm:@supabase/supabase-js@2";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_PROJECT_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_API_KEY;
